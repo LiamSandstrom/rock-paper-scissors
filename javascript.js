f703f0f6915f261aa699852c28f30964edf57d58
LiamSandstrom <113420205+LiamSandstrom@users.noreply.github.com>
2025-04-01 01:02:45 +0200

    add getComputerChoice and values array

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-console.log("hi");
+const log = console.log;
+const values = ["rock", "paper", "scissors"];
+
+
+function getComputerChoice(){
+    let val = Math.floor((Math.random() * 3));
+    return values[val];
+}
+
+
+function getHumanChoice(){
+    return prompt("What ya got? (rock) (paper) (sicssors)").toLowerCase();
+}
