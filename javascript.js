e4525fcd37331c66c6f8a520893e324437e84be1
LiamSandstrom <113420205+LiamSandstrom@users.noreply.github.com>
2025-04-01 01:33:54 +0200

    add playRound function
    changed array to hashmap so make winner calc
    much easier!

--- javascript.js
+++ javascript.js
@@ -1,13 +1,40 @@
 const log = console.log;
-const values = ["rock", "paper", "scissors"];
+const values = new Map();
+
+values.set("rock", "scissors");
+values.set("paper", "rock");
+values.set("scissors", "paper");
+
 let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice(){
     let val = Math.floor((Math.random() * 3));
-    return values[val];
+    if(val === 0){
+        return "rock";
+    }
+    else if(val === 1){
+        return "paper";
+    }
+    else{
+        return "scissors";
+    }
 }
 
 function getHumanChoice(){
-    return prompt("What ya got? (rock) (paper) (sicssors)").toLowerCase();
+    return prompt("What ya got? (rock) (paper) (scissors)");
+}
+
+playRound(getHumanChoice(), getComputerChoice());
+
+function playRound(player, computer){
+    if(player === computer){
+        log("draw");
+    }
+    else if(values.get(player) === computer){
+        log("You Win :) " + player + " beats " + computer);
+    } 
+    else{
+        log("You Lose :( " + computer + " beats " + player);
+    }
 }
