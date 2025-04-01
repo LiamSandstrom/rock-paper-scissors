76eb574fcb12aa6f430b1ffe9a8735bf0b2bbf71
LiamSandstrom <113420205+LiamSandstrom@users.noreply.github.com>
2025-04-01 03:09:30 +0200

    change if statment to switch in getComputerChoice

--- javascript.js
+++ javascript.js
@@ -40,14 +40,13 @@ function playRound(){
 
 function getComputerChoice(){
     let val = Math.floor((Math.random() * 3));
-    if(val === 0){
-        return "rock";
-    }
-    else if(val === 1){
-        return "paper";
-    }
-    else{
-        return "scissors";
+    switch (val){
+        case(0):
+            return "rock";
+        case(1):
+            return "paper";
+        case(2): 
+            return "scissors";
     }
 }
 
