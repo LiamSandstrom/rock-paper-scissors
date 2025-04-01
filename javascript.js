590c7f9c65bbda566a5dbc2d3f31e88f1e7093fd
LiamSandstrom <113420205+LiamSandstrom@users.noreply.github.com>
2025-04-01 02:08:27 +0200

    add logResult helper function

--- javascript.js
+++ javascript.js
@@ -1,12 +1,41 @@
+//for easier log while coding
 const log = console.log;
-const values = new Map();
 
+//HashMap with <Move : beats>
+const values = new Map();
 values.set("rock", "scissors");
 values.set("paper", "rock");
 values.set("scissors", "paper");
 
 let humanScore = 0;
 let computerScore = 0;
+let rounds = 5;
+
+playGame();
+
+function playGame(){
+    for(let i = 0; i < rounds; i++){
+        playRound();
+    }
+    logResult();
+}
+
+function playRound(){
+    let player = getHumanChoice();
+    let computer = getComputerChoice();
+
+    if(player === computer){
+        log("draw");
+    }
+    else if(values.get(player) === computer){
+        log("You Win :) " + player + " beats " + computer);
+        humanScore++;
+    } 
+    else{
+        log("You Lose :( " + computer + " beats " + player);
+        computerScore++;
+    }
+}
 
 function getComputerChoice(){
     let val = Math.floor((Math.random() * 3));
@@ -22,19 +51,11 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-    return prompt("What ya got? (rock) (paper) (scissors)");
+    return prompt("What ya got? (rock) (paper) (scissors)").toLowerCase();
 }
 
-playRound(getHumanChoice(), getComputerChoice());
-
-function playRound(player, computer){
-    if(player === computer){
-        log("draw");
-    }
-    else if(values.get(player) === computer){
-        log("You Win :) " + player + " beats " + computer);
-    } 
-    else{
-        log("You Lose :( " + computer + " beats " + player);
-    }
+function logResult(){
+    humanScore > computerScore ? log("You Won " + humanScore + " : " + computerScore) : 
+    humanScore < computerScore ? log("You Lost " + humanScore + " : " + computerScore) : 
+    log("Draw " + humanScore + " : " + computerScore);
 }
