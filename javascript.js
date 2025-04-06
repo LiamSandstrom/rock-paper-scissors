773e46c3132bb660d245a1750a27ea35841e143d
LiamSandstrom <113420205+LiamSandstrom@users.noreply.github.com>
2025-04-06 14:58:11 +0200

    add images to gameplay loop

--- javascript.js
+++ javascript.js
@@ -7,17 +7,23 @@ values.set("rock", "scissors");
 values.set("paper", "rock");
 values.set("scissors", "paper");
 
+let clicked = false;
 let hover = 0;
+let countdownTimer = 0;
 const rotateVal = 0.7;
 const whiteColor = "rgb(242, 239, 239)"; 
 const backColor = "#123456";
 const playerUIScore = document.querySelector("#player-score");
 const computerUIScore = document.querySelector("#computer-score");
+const playerImage = document.querySelector("#player-image");
+const computerImage = document.querySelector("#computer-image");
+const timerText = document.querySelector("#timer");
+
 const buttons = document.querySelectorAll("button");
 for(let button of buttons){
     button.addEventListener("click", () => {
         let move = button.value;
-        playRound(move);
+        countdown(move);
     });
 
     button.addEventListener("mouseenter", () => hoverButton(button));
@@ -25,23 +31,33 @@ for(let button of buttons){
     button.addEventListener("mousedown", () => buttonDown(button));
     button.addEventListener("mouseup", () => hoverButton(button));
 }
-
+const second = 1000;
 let humanScore = 0;
 let computerScore = 0;
 let rounds = 5;
+const countdownTime = second * 3;
 
 function playGame(){
     for(let i = 0; i < rounds; i++){
         log("Round: " + (i + 1));
-        playRound();
+        playRound(input);
     }
     logResult();
 }
 
-function playRound(input){
+function countdown(move){
+    updateImage(move, "player");
+    clicked = true;
+    setTimeout(() => playRound(move), countdownTime);
+    let val = countdownTime / second;
+    timerText.textContent = val;
+    countdownTimer = setInterval(updateCountdownText, second);
+}
+
+async function playRound(input){
     let player = input;
     let computer = getComputerChoice();
-
+    updateImage(computer);
     if(player === computer){
         log("draw");
     }
@@ -88,6 +104,8 @@ function hoverButton(button){
     button.style.backgroundColor = backColor;
     button.style.color = whiteColor;
     button.style.border = `2px solid ${whiteColor}`;
+    let move = button.value;
+    updateImage(move,"player", "20%");
 }
 
 async function hoverButtonTick(button, val){
@@ -110,14 +128,57 @@ function stopHoverButton(button){
     button.style.color = backColor;
     button.style.border = "none";
     clearTimeout(hover);
+    resetImage("player");
 }
 
 function buttonDown(button){
-
     button.style.transform = "scale(1.0)";
     clearTimeout(hover);
     button.style.backgroundColor = "rgb(118, 175, 11)";
 }
+
+function updateImage(move, target = "", opacity = "100%"){
+    if(target === "player"){
+        if(clicked === true) return;
+        target = playerImage;
+    }
+    else{
+        target = computerImage;
+        console.log("computa");
+    }
+    target.src = `${move}.png`;
+    target.style.opacity = opacity;
+}
+
+function resetImage(target){
+    if(clicked === true) return;
+    if(target === "player"){
+        target = playerImage;
+        target.style.opacity = "0%";
+    }
+    else{
+        target = computerImage;
+        target.style.opacity = "50%";
+        target.src = "question-mark.png";
+    }
+}
+
+async function updateCountdownText(){
+    let val = Number.parseInt(timerText.textContent);
+    val -= 1;
+    if(val === 0){
+        timerText.textContent = "VS";
+        resetTimer();
+        return;
+    }
+    timerText.textContent = val;
+}
+
+function resetTimer(){
+    clearInterval(countdownTimer);
+}
+
+
 
 
 // Plan:
